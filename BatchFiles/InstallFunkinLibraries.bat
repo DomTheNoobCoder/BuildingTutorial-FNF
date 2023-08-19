4a037b2133fa573ec63a244a8de7be21b07d30c9
DomTheTechMaster <101476659+DomTheNoobCoder@users.noreply.github.com>
2023-08-19 12:19:19 -0700

    Create InstallFunkinLibraries.bat

---
+++ BatchFiles/InstallFunkinLibraries.bat
@@ -0,0 +1,13 @@
+haxelib install lime 7.9.0
+haxelib install openfl
+haxelib install hscript
+haxelib install actuate
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit.git
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git polymod https://github.com/MasterEric/polymod.git
+haxelib git extension-webm https://github.com/KadeDev/extension-webm
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc.git
+lime rebuild extension-webm window
+haxelib install hmm
+haxelib run hmm install
