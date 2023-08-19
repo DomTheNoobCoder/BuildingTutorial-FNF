23cdea1040bab03a80fe8e5529006b7e14ac9b1f
DomTheTechMaster <101476659+DomTheNoobCoder@users.noreply.github.com>
2023-08-19 12:15:00 -0700

    Update HaxeFlixel.bat

--- BatchFiles/HaxeFlixel.bat
+++ BatchFiles/HaxeFlixel.bat
@@ -2,7 +2,10 @@ haxelib install lime
 haxelib install openfl
 haxelib install flixel
 haxelib run lime setup flixel
+haxelib install flixel-addons
+haxelib install flixel-ui
 haxelib run lime setup
 haxelib install flixel-tools
 haxelib run flixel-tools setup
+haxelib run lime setup flixel
 haxelib update flixel
